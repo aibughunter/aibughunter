9bb1e19eb111581b74a3beb39041a7447c6e1bc6
UnciaBit <unciabit@gmail.com>
2022-07-27 01:46:35 +1000

    Added model download +  formatted debug

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
+import { rejects } from 'assert';
 import { setFlagsFromString } from 'v8';
 import * as vscode from 'vscode';
-import {Config, DownloadURLs, HighlightTypes, InferenceModes, InformationLevels} from './config';
+import {Config, DebugTypes, DownloadURLs, HighlightTypes, InferenceModes, InformationLevels} from './config';
 
-// const axios = require('axios');
+const axios = require('axios');
 const fs = require('fs');
-const Path = require('path');
+const path = require('path');
 const fsa = require('fs/promises');
 // const formdata = require('form-data');
 // const extract = require('extract-zip');
@@ -18,7 +19,22 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from AIBugHunter!');
 	});
 
+	debugMessage(DebugTypes.info, "Extension activated");
+
 	intialiseConfig();
+
+	debugMessage(DebugTypes.info, "Config loaded, checking model presence");
+
+	modelInit().then(() => {
+		debugMessage(DebugTypes.info, "Model successfully loaded");
+	}
+	).catch(err => {
+		debugMessage(DebugTypes.error, err);
+		// console.log(err);
+	}
+	);
+
+
 	
 	context.subscriptions.push(disposable);
 }
@@ -49,4 +65,72 @@ function intialiseConfig(){
 		cwePath: vsconfig.cWE.downloadLocation,
 		// cweURL: DownloadURLs.cweList
 	};
+}
+
+/**
+ * Check if the model is downloaded and download if not
+ */
+ async function modelInit(){
+	const lineModelPath = path.resolve(__dirname,config.modelPath, 'line_model.onnx');
+	const sevModelPath = path.resolve(__dirname,config.modelPath, 'sev_model.onnx');
+	const cweModelPath = path.resolve(__dirname,config.modelPath, 'cwe_model.onnx');
+
+	var downloads = [];
+
+	if(!fs.existsSync(lineModelPath)){
+		debugMessage(DebugTypes.info, "line_model not found, downloading...");
+		downloads.push(downloadEngine(fs.createWriteStream(lineModelPath), DownloadURLs.lineModel));
+	}
+	
+	if(!fs.existsSync(sevModelPath)){
+		debugMessage(DebugTypes.info, "sve_model not found, downloading...");
+		downloads.push(downloadEngine(fs.createWriteStream(sevModelPath), DownloadURLs.sevModel));
+	}
+
+	if(!fs.existsSync(cweModelPath)){
+		debugMessage(DebugTypes.info, "cwe_model not found, downloading...");
+		// downloads.push(downloadEngine(fs.createWriteStream(cweModelPath), DownloadURLs.cweModel));
+	}
+
+	await Promise.all(downloads).then(() => {	
+		debugMessage(DebugTypes.info, "All missing models downloaded");	
+		return Promise.resolve();
+	}
+	).catch(err => {
+		debugMessage(DebugTypes.error, "Error occured while downloading models");
+		return Promise.reject(err);
+	}
+	);
+}
+
+/**
+ * Downloads stream from specified URL then writes to file
+ * @param writer Stream to write to
+ * @param url Download URL
+ * @returns Promise that resolves when download is complete
+ */
+async function downloadEngine(writer:any, url: string){
+
+	const response = await axios({
+		method: 'GET',
+		url: url,
+		responseType: 'stream'
+	});
+	
+	response.data.pipe(writer);
+
+	return new Promise((resolve, reject) => {
+		writer.on('finish', resolve);
+		writer.on('error', reject);
+	});
+}
+
+/**
+ * Print formatted debug message to console
+ * @param type Type of message to log (Info, Error)
+ * @param message Message to display
+ */
+function debugMessage(type:string, message:string){
+	// Print with ISO date
+	console.log("[" + type + "] [" + new Date().toISOString() + "] " + message);
 }
